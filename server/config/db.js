af9e9b5a5b8d6e8aebbf6c4cf5562819e62fc2a8
propiccc <alexandronapoleon01#gmail.com>
2025-06-29 15:18:36 +0700

    update docker composer

--- server/config/db.js
+++ server/config/db.js
@@ -18,7 +18,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
-  } catch (err) {
+  } catch (err) { 
     console.error("MongoDB connection error:", err);
     process.exit(1);
   }
