1846bcbbfca4d6f3d76e4a5d025d86f3a3de6138
mysecondpride <englishbybudi@gmail.com>
2025-07-15 07:24:34 +0700

    updated seo

--- server/models/Post.js
+++ server/models/Post.js
@@ -21,64 +21,7 @@ const Post = new Schema({
       title: { type: String },
       content: { type: String },
       url: { type: String },
-<<<<<<< HEAD
-      // files: [
-      //   {
-      //     fileId: {
-      //       type: mongoose.Schema.Types.ObjectId,
-      //     },
-      //     filename: { type: String },
-      //     fileType: { type: String },
-      //     url: { type: String }, // e.g., local path or cloud URL
-      //     uploadedAt: { type: Date, default: Date.now },
-      //   },
-      // ],
     },
   ],
-=======
-      files: [
-        fileSchema
-      ],
-    },
-  ],
-
-  element2: [
-    {
-      body2: { type: String },
-
-      files: [
-       fileSchema
-      ], // Embedding the file schema here
-    },
-  ],
-
-  element3: [
-    {
-      body3: { type: String },
-      url: { type: String },
-      files: [
-       fileSchema
-      ], // Embedding the file schema here
-    },
-  ],
-
-  element4: [
-    {
-      body4: { type: String },
-
-      files: [
-       fileSchema
-      ], // Embedding the file schema here
-    },
-  ],
-
-  penutup: [
-    {
-      penutup: { type: String },
-    },
-  ],
-
-  // Embedding the file schema here
->>>>>>> c1086d8f0773ddd80fa9e649c3310998a8bd9b1d
 });
 module.exports = mongoose.model("Post", Post);
