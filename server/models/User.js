55614aa2d2489b3648f3c723cae415307fa4c363
mysecondpride <englishbybudi@gmail.com>
2025-07-16 10:35:57 +0700

    finish sayuranku

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
   },
 });
 
---
+++ server/seeder/creteUser.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
