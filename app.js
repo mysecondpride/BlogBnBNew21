e754dcf4fd7b8d0d4b33882b5de056855f08bb65
mysecondpride <englishbybudi@gmail.com>
2026-04-04 21:10:18 +0700

    tabarokallah

--- app.js
+++ app.js
@@ -104,9 +104,9 @@ const startServer = async () => {
     })
   );
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 };
 
 startServer();
